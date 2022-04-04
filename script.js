a0c242d73b12ba883e44b5c47cf84be1812e270e
Yonghang Wu <wuyonghang12138@gmail.com>
2022-04-04 17:00:56 +0800

    优化调用api时的异常处理

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const newQuoteBtn = document.querySelector('#new-quote');
 const twitterBtn = document.querySelector('#twitter');
 const loader = document.querySelector('#loader');
 
+let errorCounter=0;
 let apiQuotes = [];
 newYiyan();
 // getQuotes();
@@ -35,7 +36,7 @@ function newQuote() {
 }
 
 // ---------------------------------
-// 从api中获取一组名人名言存到apiQuotes
+// 从quotable api中获取一组名人名言存到apiQuotes
 // ----------------------------------
 async function getQuotes() {
   try {
@@ -64,43 +65,51 @@ function newTweet() {
 // --------------
 async function newYiyan() {
   try {
-    loading();
+    showLoadingSpinner();
     const response = await fetch("https://v1.hitokoto.cn?c=d&c=h&c=i");
     if (response.ok) {
       const quote = await response.json();
       
       quoteText.textContent = quote.hitokoto;
-      // 检查作者是否为空，如果是替换成名
+      // 检查作者是否为空，如果是替换佚名
       if(!quote.from_who) {
         quote.from_who = '佚名';
       }
       
-      // 过长的文字缩小字号
-      if (quote.hitokoto.length > 80) {
+      // 过长的文字则缩小字号
+      if (quote.hitokoto.length > 30) {
         quoteText.classList.add("quote__text--long");
       } else {
         quoteText.classList.remove("quote__text--long")
       }
 
+      //成功写入
       authorText.textContent = quote.from_who + "《"+quote.from+"》";
-      complete();
+      errorCounter=0;
+      removeLoadingSpinner();
+
     } else {
       throw new Error(response.status);
     }
   } catch (e) {
     console.log(e);
+    // 出错则最多再重试十次
+    if(errorCounter>=10) {
+      removeLoadingSpinner();
+      quoteContainer.innerHTML="出问题了！要不刷新试试 ◑﹏◐";
+    } else {
+      newYiyan();
+      errorCounter++;
+    }
   }
 }
 
-// =======
-// 等待画面
-// =======
-function loading() {
+function showLoadingSpinner() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-function complete() {
+function removeLoadingSpinner() {
   loader.hidden = true;
   quoteContainer.hidden = false;
 }
